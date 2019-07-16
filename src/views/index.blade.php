2f09184c96eaf8347716d608f4975f29014f006a
Armand Grundmanis <agrundmanis@inbox.lv>
2019-07-17 01:33:36 +0300

    updated template

--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -1,40 +1,68 @@
-@extends('laracms.dashboard::layouts.app', ['page' => __('admin.menu.banners')])
+@extends(view()->exists('laracms.dashboard.layouts.app') ? 'laracms.dashboard.layouts.app' : 'laracms.dashboard::layouts.app', ['page' => __('laracms::admin.menu.banners')] )
 
 @section('content')
     <div class="form-group">
-        <a class="btn btn-success" href="{{ route('laracms.banners.create') }}">{{ __('admin.create') }}</a>
+        <a class="btn btn-success" href="{{ route('laracms.banners.create') }}">{{ __('laracms::admin.create') }}</a>
     </div>
 
-    <div class="table-responsive">
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>{{ __('admin.id') }}</th>
-                    <th>{{ __('admin.image') }}</th>
-                    <th>{{ __('admin.placement') }}</th>
-                    <th>{{ __('admin.link') }}</th>
-                    <th>{{ __('admin.created_at') }}</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-            @foreach($banners as $banner)
-                <tr>
-                    <td>{{ $banner->id }}</td>
-                    <td><img width="100" src="{{ asset($banner->image) }}" alt=""></td>
-                    <td>{{ $banner->placement }}</td>
-                    <td>@if ($banner->link)<a href="{{ $banner->link }}">{{ $banner->link }}</a>@endif</td>
-                    <td>{{ $banner->created_at }}</td>
-                    <td>
-                        <a href="{{ route('laracms.banners.edit', $banner->id) }}">{{ __('admin.edit') }}</a>
-                        |
-                        <a onclick="return confirm('Are you sure?')"
-                           href="{{ route('laracms.banners.destroy', $banner->id) }}">{{ __('admin.delete') }}</a>
-                    </td>
-                </tr>
-            @endforeach
-            </tbody>
-        </table>
-        {{ $banners->links() }}
+    <div class="content">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title">{{ __('laracms::admin.laracms_users') }}</h4>
+                    </div>
+                    <div class="card-body">
+                        @if (count($banners))
+                            <div class="table-responsive">
+                                <table class="table">
+                                    <thead class="text-primary">
+                                    <tr>
+                                        <th>#</th>
+                                        <th>{{ __('laracms::admin.image') }}</th>
+                                        <th>{{ __('laracms::admin.placement') }}</th>
+                                        <th>{{ __('laracms::admin.link') }}</th>
+                                        <th>{{ __('laracms::admin.created_at') }}</th>
+                                        <th>{{ __('laracms::admin.actions') }}</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($banners as $banner)
+                                        <tr>
+                                            <td>{{ $banner->id }}</td>
+                                            <td><img width="100" src="{{ asset($banner->image) }}" alt=""></td>
+                                            <td>{{ $banner->placement }}</td>
+                                            <td>@if ($banner->link)<a href="{{ $banner->link }}">{{ $banner->link }}</a>@endif</td>
+                                            <td>{{ $banner->created_at }}</td>
+                                            <td class="text-right">
+                                                <a title="{{ __('laracms::admin.edit') }}"
+                                                   href="{{ route('laracms.banners.edit', $banner->id) }}"
+                                                   rel="tooltip" class="btn btn-success btn-icon btn-sm ">
+                                                    <i class="fa fa-edit"></i>
+                                                </a>
+                                                <a title="{{ __('laracms::admin.delete') }}"
+                                                   onclick="return confirmDelete('{{ route('laracms.banners.destroy', $banner->id) }}');"
+                                                   href="javascript:void(0);" type="button"
+                                                   rel="tooltip" class="btn btn-danger btn-icon btn-sm ">
+                                                    <i class="fa fa-times"></i>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                                {{ $banners->links() }}
+                            </div>
+                        @else
+                            <blockquote>
+                                <p class="blockquote blockquote-primary">
+                                    {{ __('laracms::admin.no_records') }}
+                                </p>
+                            </blockquote>
+                        @endif
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
